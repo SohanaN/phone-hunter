f490668a081229c495f320ecdc16c81f74a0e9c8
SohanaN <sohananaz01@gmail.com>
2022-03-02 04:10:56 +0600

    show phone

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
+const showPhone = document.getElementById("sn-phone");
 const smartPhone = () => {
     const searchValue = document.getElementById("sn-search-box").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data));
-    console.log(url);
+        .then((data) => showSmartPhoneDetails(data.data));
+};
+const showSmartPhoneDetails = (phones) => {
+    for (const phone of phones) {
+        console.log(phone);
+        const div = document.createElement("div");
+        div.classList.add("col-lg-4");
+        div.classList.add("col-sm-12");
+        div.classList.add("mb-5");
+        div.innerHTML = `
+            <div class="card" style="width: 18rem;">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                    <h6 class="card-title">Brand: ${phone.brand}</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Details</a>
+                </div>
+            </div>
+            `
+        showPhone.appendChild(div);
+    }
 }
