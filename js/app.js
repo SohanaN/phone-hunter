4a007b06f4d9f365a0a78cfea23c0c7e60dca0f6
SohanaN <sohananaz01@gmail.com>
2022-03-02 09:08:48 +0600

    showed in middle

--- js/app.js
+++ js/app.js
@@ -58,9 +58,10 @@ const phoneDetails = (id) => {
 }
 // show phone details 
 const displayPhoneDetail = singlePhone => {
-    console.log(singlePhone);
+    // console.log(singlePhone);
+    const showPhoneDetails = document.getElementById('sn-phone-details');
     const div = document.createElement("div");
-    showPhone.innerHTML = "";
+    showPhoneDetails.innerHTML = "";
     div.classList.add("my-5");
     div.classList.add("d-flex");
     div.classList.add("justify-content-center");
@@ -71,27 +72,29 @@ const displayPhoneDetail = singlePhone => {
                     <h4 class="card-title">Name: ${singlePhone.name}</h4>
                     <h5 class="card-title">Brand: ${singlePhone.brand}</h5> 
                     <h6 class="card-title">Release Date: ${singlePhone.releaseDate}</h6> 
-                    <div>
-                        <h5><b>Main Features:</b></h5>
-                        <p><b>Storage</b>: ${singlePhone.mainFeatures.storage}</p> 
-                        <p><b>DisplaySize</b>: ${singlePhone.mainFeatures.displaySize}</p> 
-                        <p><b>ChipSet</b>: ${singlePhone.mainFeatures.chipSet}</p> 
-                        <p><b>Memory</b>: ${singlePhone.mainFeatures.memory}</p> 
-                        <p><b>Sensors</b>: ${singlePhone.mainFeatures.sensors}</p> 
-                    </div>
-                    <div>
-                        <h5><b>Others:</b></h5>
-                        <p><b>Bluetooth</b>: ${singlePhone.others.Bluetooth}</p> 
-                        <p><b>GPS</b>: ${singlePhone.others.GPS}</p> 
-                        <p><b>NFC</b>: ${singlePhone.others.NFC}</p> 
-                        <p><b>Radio</b>: ${singlePhone.others.Radio}</p> 
-                        <p><b>USB</b>: ${singlePhone.others.USB}</p> 
-                        <p><b>WLAN</b>: ${singlePhone.others.WLAN}</p> 
-                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h5><b>Main Features:</b></h5>
+                            <p><b>Storage</b>: ${singlePhone.mainFeatures.storage}</p> 
+                            <p><b>DisplaySize</b>: ${singlePhone.mainFeatures.displaySize}</p> 
+                            <p><b>ChipSet</b>: ${singlePhone.mainFeatures.chipSet}</p> 
+                            <p><b>Memory</b>: ${singlePhone.mainFeatures.memory}</p> 
+                            <p><b>Sensors</b>: ${singlePhone.mainFeatures.sensors}</p> 
+                        </div>
+                        <div class="col">
+                            <h5><b>Others:</b></h5>
+                            <p><b>Bluetooth</b>: ${singlePhone.others.Bluetooth}</p> 
+                            <p><b>GPS</b>: ${singlePhone.others.GPS}</p> 
+                            <p><b>NFC</b>: ${singlePhone.others.NFC}</p> 
+                            <p><b>Radio</b>: ${singlePhone.others.Radio}</p> 
+                            <p><b>USB</b>: ${singlePhone.others.USB}</p> 
+                            <p><b>WLAN</b>: ${singlePhone.others.WLAN}</p> 
+                        </div>
+                    </div
                 </div>
             </div>
             `
-    showPhone.appendChild(div);
+    showPhoneDetails.appendChild(div);
 
 }
 
